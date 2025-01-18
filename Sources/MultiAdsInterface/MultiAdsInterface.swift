9f3526f57c8a41d90c4c944b7611e54f13aa7e72
Coderrb123 <devloperrb73375@gmail.com>
2025-01-18 18:40:03 +0530

    first commit

--- Sources/MultiAdsInterface/MultiAdsInterface.swift
+++ Sources/MultiAdsInterface/MultiAdsInterface.swift
@@ -14,13 +14,14 @@ public class MultiAdsInterface {
     
     
     @MainActor public func setUp() {
-        
-        let identifier:UUID? =  AppTrans().getTrackingIdentifierWithRequest()
-        ApiReposiotry().deviceRegister(adId:identifier?.uuidString ?? "00000000-0000-0000-0000-000000000000",rewardType: 1) { deviceID in
-        
-        } onError: { ErrorString in
-            
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            let identifier:UUID? =  AppTrans().getTrackingIdentifierWithRequest()
+            ApiReposiotry().deviceRegister(adId:identifier?.uuidString ?? "00000000-0000-0000-0000-000000000000",rewardType: 1) { deviceID in
+            } onError: { ErrorString in
+                
+            }
         }
+       
     }
     
 
