6ef7644084171e729a83a6ff343bd462373edd55
Coderrb123 <devloperrb73375@gmail.com>
2025-02-01 01:00:23 +0530

    Yooo

--- Sources/MultiAdsInterface/Helpers/AppAdHelper.swift
+++ Sources/MultiAdsInterface/Helpers/AppAdHelper.swift
@@ -2,17 +2,30 @@
 //  SwiftUIView.swift
 //  MultiAdsInterface
 //
-//  Created by Khusnud Zehra on 01/02/25.
+//  Created by Khusnud Zehra on 01/02/25 For.
 //
 
 import SwiftUI
 
-struct SwiftUIView: View {
+@available(iOS 13.0, *)
+struct AppAdHelper<Content: View>: View {
+    
+    let content: () -> Content
+    @State var notFirstTime: Bool = false
+    let registerAppParameters:RegisterAppParameters
+    init(@ViewBuilder content: @escaping () -> Content,registerAppParameters: RegisterAppParameters) {
+        self.content = content
+        self.registerAppParameters = registerAppParameters
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        content().onAppear {
+            if(!notFirstTime){
+                MultiAdsInterface().setUp(
+                    registerAppParameters: registerAppParameters
+                )
+            }
+        }
     }
 }
 
-#Preview {
-    SwiftUIView()
-}
+
