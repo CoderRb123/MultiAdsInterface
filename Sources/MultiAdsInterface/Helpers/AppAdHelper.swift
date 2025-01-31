aaf9531c3145c2c8543afca067e576f9d5078c2c
Coderrb123 <devloperrb73375@gmail.com>
2025-02-01 00:59:11 +0530

    Added Helper

---
+++ Sources/MultiAdsInterface/Helpers/AppAdHelper.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MultiAdsInterface
+//
+//  Created by Khusnud Zehra on 01/02/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
