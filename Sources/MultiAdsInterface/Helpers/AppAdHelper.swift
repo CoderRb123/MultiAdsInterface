c4ddb39fe42b888ba5fe12cd021ef030041606e3
Coderrb123 <devloperrb73375@gmail.com>
2025-03-25 18:41:04 +0530

    changed

--- Sources/MultiAdsInterface/Helpers/AppAdHelper.swift
+++ Sources/MultiAdsInterface/Helpers/AppAdHelper.swift
@@ -12,20 +12,20 @@ public struct AppAdHelper<Content: View>: View {
     let content: () -> Content
     @State var notFirstTime: Bool = false
     let registerAppParameters:RegisterAppParameters
-    let networkLinks: NetworkLinks
+    let baseUrl: String
     
     @ObservedObject var networkConstvariable:NetworkConst = NetworkConst.shared
     
-    public init(@ViewBuilder content: @escaping () -> Content,registerAppParameters: RegisterAppParameters,networkConst:NetworkLinks) {
+    public init(@ViewBuilder content: @escaping () -> Content,registerAppParameters: RegisterAppParameters,baseUrl:String) {
         self.content = content
         self.registerAppParameters = registerAppParameters
-        self.networkLinks = networkConst
+        self.baseUrl = baseUrl
     }
     public var body: some View {
         content().onAppear {
             if(!notFirstTime){
                 
-                networkConstvariable.networkLink = networkConstvariable.networkLink
+                networkConstvariable.networkLink.baseURL = baseUrl
                 MultiAdsInterface().setUp(
                     registerAppParameters: RegisterAppParameters(
                         
